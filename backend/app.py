67d7f5418dad8c0e80198f640ce0d7260e29b158
Aparmit <aparmitsrivastav01@gmail.com>
2026-04-12 10:57:29 +0530

    fix: render port binding

--- backend/app.py
+++ backend/app.py
@@ -27,6 +27,8 @@ import pandas as pd
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+import os 
+
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
@@ -516,4 +518,5 @@ def upload_xlsx():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
